ce255fa5ebe0f0befa525bd60a80e313a76e65ab
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-11 14:52:59 -0700

    Seeding our application with development data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# === NOTE: FROM JECODE93 ===
+
+# If you like using fixtures for your tests, you may know that instead of running
+# bin/rails db:seed you can run bin/rails db:fixtures:load to create
+# development data from your fixtures files. Let's tell Rails that the two
+#  commands are equivalent in the db/seeds.rb file:
+
+puts "\n== Seeding the database with fixtures =="
+system('bin/rails db:fixtures:load')
