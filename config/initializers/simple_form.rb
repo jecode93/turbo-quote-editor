7d72119b3a9eea4790fb153c3be82433d2a87336
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-13 22:17:44 -0400

    Rubocop change.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # config/initializers/simple_form.rb
 
 SimpleForm.setup do |config|
--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,37 +1,39 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class QuotesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get quotes_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get quotes_show_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get quotes_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get quotes_create_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get quotes_edit_url
     assert_response :success
   end
 
-  test "should get update" do
+  test 'should get update' do
     get quotes_update_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get quotes_destroy_url
     assert_response :success
   end
