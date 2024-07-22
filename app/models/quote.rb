c65679a67908719a223f51aab2ef86b68c978c88
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-22 10:07:16 -0400

    Broadcasting quote updates with Turbo Streams.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,4 +7,5 @@ class Quote < ApplicationRecord
   # Ex:- scope :active, -> {where(:active => true)}
 
   after_create_commit -> { broadcast_prepend_to 'quotes' }
+  after_update_commit -> { broadcast_replace_to 'quotes' }
 end
