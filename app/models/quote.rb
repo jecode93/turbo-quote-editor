b26dcc20b07c5cf84ffcfbbcfc95a376950ba447
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-22 09:51:04 -0400

    Broadcasting created quotes with Turbo Streams.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,6 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
   # Ex:- scope :active, -> {where(:active => true)}
+
+  after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self }, target: 'quotes' }
 end
