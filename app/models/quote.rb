fa4310d2f6b5b0e67897ebe3dd87a2b7bff74744
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-22 10:29:57 -0400

    Making broadcasting asynchronous with ActiveJob.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,7 +6,7 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
   # Ex:- scope :active, -> {where(:active => true)}
 
-  after_create_commit -> { broadcast_prepend_to 'quotes' }
-  after_update_commit -> { broadcast_replace_to 'quotes' }
+  after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  after_update_commit -> { broadcast_replace_later_to 'quotes' }
   after_destroy_commit -> { broadcast_remove_to 'quotes' }
 end
