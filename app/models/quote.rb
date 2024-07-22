2c92acea87d9ff2cee23a28dea6a717e761adb68
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-22 11:11:39 -0400

    More syntactic sugar.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,7 +6,9 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
   # Ex:- scope :active, -> {where(:active => true)}
 
-  after_create_commit -> { broadcast_prepend_later_to 'quotes' }
-  after_update_commit -> { broadcast_replace_later_to 'quotes' }
-  after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks above  are equivalent to the following single line
+  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
 end
