74cc5db4a852f473492d2f79267dbca91745a7f7
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-22 10:16:30 -0400

    Broadcasting quote deletion with Turbo Streams.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,4 +8,5 @@ class Quote < ApplicationRecord
 
   after_create_commit -> { broadcast_prepend_to 'quotes' }
   after_update_commit -> { broadcast_replace_to 'quotes' }
+  after_destroy_commit -> { broadcast_remove_to 'quotes' }
 end
