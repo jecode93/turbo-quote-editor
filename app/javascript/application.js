323728265fd6fcb034b28fb28dd2e4ed31829205
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-29 08:58:25 -0400

    Adding flash messages to our CRUD controller.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+
+// app/javascript/application.js
+
 import "./controllers"
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
