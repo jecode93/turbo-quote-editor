7e8c549cc7a5cb04668303c7a1098a1e493f2616
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-08-01 22:07:18 -0400

    Updating line item dates without Turbo.

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,5 +1,6 @@
 class LineItemDatesController < ApplicationController
   before_action :set_quote
+  before_action :set_line_item_date, only: %i[edit update]
 
   def new
     @line_item_date = @quote.line_item_dates.build
@@ -9,13 +10,28 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was successfully created."
+      redirect_to quote_path(@quote), notice: 'Date was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: 'Date was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:id])
+  end
 
   def line_item_date_params
     params.require(:line_item_date).permit(:date)
