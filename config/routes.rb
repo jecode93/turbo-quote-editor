9823ab0f30e3b94505f33aab2a08e33a314e27f1
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-08-01 21:31:57 -0400

    Adding routes for line item dates.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: %i[index show]
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
