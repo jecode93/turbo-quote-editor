394ade7d2798aca491b425b91bfd682b493f89dc
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-08-17 20:11:50 -0400

    Create the model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_802_012_007) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_17_235326) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'companies', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'line_item_dates', force: :cascade do |t|
-    t.bigint 'quote_id', null: false
-    t.date 'date', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[date quote_id], name: 'index_line_item_dates_on_date_and_quote_id', unique: true
-    t.index ['date'], name: 'index_line_item_dates_on_date'
-    t.index ['quote_id'], name: 'index_line_item_dates_on_quote_id'
+  create_table "line_item_dates", force: :cascade do |t|
+    t.bigint "quote_id", null: false
+    t.date "date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["date", "quote_id"], name: "index_line_item_dates_on_date_and_quote_id", unique: true
+    t.index ["date"], name: "index_line_item_dates_on_date"
+    t.index ["quote_id"], name: "index_line_item_dates_on_quote_id"
   end
 
-  create_table 'quotes', force: :cascade do |t|
-    t.string 'name', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'company_id', null: false
-    t.index ['company_id'], name: 'index_quotes_on_company_id'
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "line_item_date_id", null: false
+    t.string "name", null: false
+    t.text "description"
+    t.integer "quantity", null: false
+    t.decimal "unit_price", precision: 10, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["line_item_date_id"], name: "index_line_items_on_line_item_date_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'company_id', null: false
-    t.index ['company_id'], name: 'index_users_on_company_id'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "quotes", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
   end
 
-  add_foreign_key 'line_item_dates', 'quotes'
-  add_foreign_key 'quotes', 'companies'
-  add_foreign_key 'users', 'companies'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "line_item_dates", "quotes"
+  add_foreign_key "line_items", "line_item_dates"
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
