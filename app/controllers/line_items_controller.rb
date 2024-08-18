6ce63f031c628a1cf6c3b0e74794a7cb301e349e
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-08-17 20:48:54 -0400

    Update line items without Turbo.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,6 +3,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: %i[edit update destroy]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -12,13 +13,27 @@ class LineItemsController < ApplicationController
     @line_item = @line_item_date.line_items.build(line_item_params)
 
     if @line_item.save
-      redirect_to quote_path(@quote), notice: "Item was successfully created."
+      redirect_to quote_path(@quote), notice: 'Item was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit; end
+
+  def update
+    if @line_item.update(line_item_params)
+      redirect_to quote_path(@quote), notice: 'Item was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find(params[:id])
+  end
 
   def line_item_params
     params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
